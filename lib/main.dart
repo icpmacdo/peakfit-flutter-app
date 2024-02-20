00a9f8fce858af03d16413309f22ef3e46c64b96
ian <icpmacdo@gmail.com>
2024-02-20 13:38:22 -0800

    Application, scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+/// This file contains the main entry point of the Flutter application.
+/// It imports necessary packages for building UI, making HTTP requests, and handling JSON data.
+/// It also imports the `intl` package for formatting dates and numbers.
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // Import the http package
 import 'dart:convert'; // For handling JSON data
+import 'package:intl/intl.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'API Flutter App',
+      title: 'PeakFit',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,47 +34,97 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _apiResponse = '';
 
-  void _makeApiRequest() async {
-    // Replace with your actual API endpoint
-    final url = Uri.parse('https://api.example.com/data'); 
-    final response = await http.get(url);
+  void _makeApiRequest(location) async {
+    print(location);
+    
+    // final url = Uri.parse('https://api.example.com/data'); 
+    // final response = await http.get(url);
 
-    setState(() {
-      if (response.statusCode == 200) {
-        _apiResponse = jsonDecode(response.body)['your_data_field'];
-      } else {
-        _apiResponse = 'Error: ${response.statusCode}';
-      }
-    });
+    // setState(() {
+    //   if (response.statusCode == 200) {
+    //     _apiResponse = jsonDecode(response.body)['your_data_field'];
+    //   } else {
+    //     _apiResponse = 'Error: ${response.statusCode}';
+    //   }
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('API Flutter App'),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_apiResponse), 
-            ElevatedButton(
-              onPressed: _makeApiRequest,
-              child: const Text('Fetch Data'),
+        // title is todays date in plain english
+        title: Text(DateFormat('MMMM d').format(DateTime.now()), style: TextStyle(fontSize: 24),
 
+      )),
+      body: Column( // Use Column for top-to-bottom layout
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Space items vertically
+        children: [
+          // Padding(
+          //   padding: const EdgeInsets.all(16.0), // Padding around text
+          //   child: Text(
+          //     'Welcome to PeakFit!',
+          //     style: TextStyle(fontSize: 24),
+          //   ),
+          // ),
+          // Center(
+          //   child: Text("_apiResponse friends list and their status!"),
+          // ),
+
+          // friends list and their status
+          Padding(
+            padding: const EdgeInsets.all(16.0), // Padding around text
+            child: Column(
+              children: [
+                Text(
+                  'Friends List',
+                  style: TextStyle(fontSize: 24),
+                ),
+                for (int i = 1; i <= 4; i++)
+                  Text(
+                    'Friend $i: ${i % 2 == 0 ? '🧗' : '🏋️'}',
+                    style: TextStyle(fontSize: 24),
+                  ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: _makeApiRequest,
-              child: const Text('Fetch Data 2'),
-              onHover: (value) {
-                print(value);
-              },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16.0), // Padding around buttons
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Distribute buttons evenly
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    const location = 'outside';
+                    _makeApiRequest(location);
+                  },
+                  child: const Text('🧗'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 70), 
+                    textStyle: TextStyle(fontSize: 40), // Increased font size 
+                  ),
+                ),
+                SizedBox(width: 16.0), 
+                ElevatedButton(
+                  onPressed: () {
+                    const location = 'inside';
+                    _makeApiRequest(location);
+                  },
+                  child: const Text('🏋️'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 70), 
+                    textStyle: TextStyle(fontSize: 40), // Increased font size 
+                  ),
+                  onHover: (value) {
+                    print(value);
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
